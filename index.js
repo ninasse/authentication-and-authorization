796f58a1bfdc807dc96ddb1437da5b540b309599
ninasse <58419590+ninasse@users.noreply.github.com>
2020-03-31 11:00:52 +0200

    heroku

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const config = require('./config/config');
 const userRoutes = require('./router/userRoutes');
 const productRoutes = require('./router/productRouter');
 const cookieParser = require('cookie-parser');
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
+//require('dotenv').config();
 
 const app = express();
 
